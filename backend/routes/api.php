5321277acc19ab8bede8bf17ecc382ea0be39d12
git-take777 <git.coding.life@gmail.com>
2024-09-16 19:37:19 +0900

    fix: apiのルートを変更

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [UserController::class, 'index']);
+Route::post('/register', [UserController::class, 'store']);
+Route::post('/register/profile', [UserController::class, 'profileStore']);
